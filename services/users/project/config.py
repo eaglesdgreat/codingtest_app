ee594161cf6c2835654ce1a71d3fafe30348c7c9
Eagles Emmanuel <okponobiemmanuel@gmail.com>
2022-08-06 23:32:36 +0100

    completed user services

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,15 +1,18 @@
+import os
 class BaseConfig:
     """Base configurations"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 class DevelopmentConfig(BaseConfig):
     """Development configurations"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing configurations"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     TESTING = True
     
 class ProductionConfig(BaseConfig):
     """Production configurations"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
